fb31ddd58ced465d4d30a33aa68f4ebb18acd592
ryota-09 <n.ryo950911@gmail.com>
2022-02-28 17:28:32 +0900

    oederConfirmのファイル作成

---
+++ src/views/OrderConfirm.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class OrderConfirm extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
