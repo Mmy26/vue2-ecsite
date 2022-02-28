8437da9b35935da5daeffd940ec9bdae7f10d787
kanacoyn <knkyn723@gmail.com>
2022-02-28 11:32:32 +0900

    store indexをpullするために一時保存

--- src/views/ItemDetail.vue
+++ src/views/ItemDetail.vue
@@ -11,14 +11,12 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import { Order } from "@/type/order";
-import axios from "axios";
-import { OrderItem } from "@/type/orderItem";
-import { orderTopping } from "@/type/OrderTopping";
 import { Item } from "@/type/item";
-import { Topping } from "@/type/topping";
+import axios from "axios";
+
 @Component
 export default class ItemDetail extends Vue {
+  // 商品情報を初期化
   private currentItem = new Item(
     0,
     "",
@@ -30,7 +28,35 @@ export default class ItemDetail extends Vue {
     false,
     new Topping(0, "", "", 0, 0)
   );
+
+  selectItem!: Item;
   private currentItemImage = "";
+
+  async created(): Promise<void> {
+    // 送られてきたリクエストパラメータのidをnumberに変換して取得する
+    const itemId = Number(this.$route.params.id);
+    // VuexストアのGetter、getItemList()メソッドに先ほど取得したIDを渡し、１件の商品情報を取得し、戻り値をselectItemに代入する
+    this.selectItem = this.$store.getters.getItemList(itemId);
+
+    const response = await axios.get(
+      "http://153.127.48.168:8080/ecsite-api/item/${itemId}"
+    );
+    console.dir("response:" + JSON.stringify(response));
+    let currentItem = response.data.item;
+    this.selectItem = new Item(
+      currentItem.id,
+      currentItem.type,
+      currentItem.name,
+      currentItem.discription,
+      currentItem.priceM,
+      currentItem.priceL,
+      currentItem.imagePath,
+      currentItem.deleted,
+      currentItem.toppingList
+    );
+    // 今取得した商品情報から画像パスを取り出し、imgディレクトリの名前を前に付与(文字列連結)してcurrentEmployeeImage属性に代入する
+    this.currentItemImage = `http://153.127.48.168:8080/ecsite-api/item/${this.selectItem.image}`;
+  }
 }
 </script>
 
