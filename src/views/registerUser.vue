e868506efd9ec1346406db202c67258aa2773170
Mmy26 <yu.b1994masamura@gmail.com>
2022-02-28 15:04:04 +0900

    エラー文の修正

--- src/views/registerUser.vue
+++ src/views/registerUser.vue
@@ -19,117 +19,115 @@
         </div>
       </div>
     </header>
-    <div class="top-wrapper">
-      <div class="container">
-        <div class="row register-page">
-          <div class="error">{{ errorMessage }}</div>
-          <div class="row">
-            <div class="error">{{ errorLastName }}</div>
-            <div class="error">{{ errorFirstName }}</div>
-            <div class="input-field col s6">
-              <input
-                id="last_name"
-                type="text"
-                class="validate"
-                v-model="lastName"
-                required
-              />
-              <label for="last_name">姓</label>
+    <form>
+      <div class="top-wrapper">
+        <div class="container">
+          <div class="row register-page">
+            <div class="error">{{ errorMessage }}</div>
+            <div class="row">
+              <div class="error">{{ errorLastName }}</div>
+              <div class="error">{{ errorFirstName }}</div>
+              <div class="input-field col s6">
+                <input
+                  id="last_name"
+                  type="text"
+                  class="validate"
+                  v-model="lastName"
+                  required
+                />
+                <label for="last_name">姓</label>
+              </div>
+              <div class="input-field col s6">
+                <input
+                  id="first_name"
+                  type="text"
+                  class="validate"
+                  v-model="firstName"
+                  required
+                />
+                <label for="first_name">名</label>
+              </div>
             </div>
-            <div class="input-field col s6">
-              <input
-                id="first_name"
-                type="text"
-                class="validate"
-                v-model="firstName"
-                required
-              />
-              <label for="first_name">名</label>
+            <div class="row">
+              <div class="error">{{ errorMailAddress }}</div>
+              <div class="input-field col s12">
+                <input
+                  id="email"
+                  type="email"
+                  class="validate"
+                  v-model="mailAddress"
+                  required
+                />
+                <label for="email">メールアドレス</label>
+              </div>
             </div>
-          </div>
-          <div class="row">
-            <div class="error">{{ errorMailAddress }}</div>
-            <div class="input-field col s12">
-              <input
-                id="email"
-                type="email"
-                class="validate"
-                v-model="mailAddress"
-                required
-              />
-              <label for="email">メールアドレス</label>
+            <div class="error">{{ errorZipCode }}</div>
+            <div class="row">
+              <div class="input-field col s12">
+                <input
+                  id="zipcode"
+                  type="text"
+                  maxlength="8"
+                  v-model="zipCode"
+                />
+                <label for="zipcode">郵便番号</label>
+                <button class="btn" type="button">
+                  <span>住所検索</span>
+                </button>
+              </div>
             </div>
-          </div>
-          <div class="error">{{ errorZipCode }}</div>
-          <div class="row">
-            <div class="input-field col s12">
-              <input
-                id="zipcode"
-                type="text"
-                maxlength="7"
-                v-model.number="zipCode"
-              />
-              <label for="zipcode">郵便番号(ハイフンなし)</label>
-              <button class="btn" type="button">
-                <span>住所検索</span>
+            <div class="row">
+              <div class="error">{{ errorAddress }}</div>
+              <div class="input-field col s12">
+                <input id="address" type="text" v-model="address" />
+                <label for="address">住所</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="error">{{ errorTelephone }}</div>
+              <div class="input-field col s12">
+                <input id="tel" type="tel" maxlength="14" v-model="telephone" />
+                <label for="tel">電話番号</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="error">{{ errorPassword }}</div>
+              <div class="input-field col s12">
+                <input
+                  id="password"
+                  type="password"
+                  class="validate"
+                  minlength="8"
+                  v-model="password"
+                  required
+                />
+                <label for="password">パスワード</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="error">{{ errorPasswordConfirmation }}</div>
+              <div class="input-field col s12">
+                <input
+                  id="confirmation_password"
+                  type="password"
+                  class="validate"
+                  minlength="8"
+                  v-model="passwordConfirmation"
+                  required
+                />
+                <label for="confirmation_password">確認用パスワード</label>
+              </div>
+            </div>
+            <div class="row register-admin-btn">
+              <button class="btn" type="button" v-on:click="registerUser">
+                <span>登録<i class="material-icons right">done</i></span>
               </button>
             </div>
-          </div>
-          <div class="row">
-            <div class="error">{{ errorAddress }}</div>
-            <div class="input-field col s12">
-              <input id="address" type="text" v-model="address" />
-              <label for="address">住所</label>
-            </div>
-          </div>
-          <div class="row">
-            <div class="error">{{ errorTelephone }}</div>
-            <div class="input-field col s12">
-              <input
-                id="tel"
-                type="tel"
-                maxlength="11"
-                v-model.number="telephone"
-              />
-              <label for="tel">電話番号</label>
-            </div>
-          </div>
-          <div class="row">
-            <div class="error">{{ errorPassword }}</div>
-            <div class="input-field col s12">
-              <input
-                id="password"
-                type="password"
-                class="validate"
-                minlength="8"
-                v-model="password"
-                required
-              />
-              <label for="password">パスワード</label>
-            </div>
-          </div>
-          <div class="row">
-            <div class="error">{{ errorPasswordConfirmation }}</div>
-            <div class="input-field col s12">
-              <input
-                id="confirmation_password"
-                type="password"
-                class="validate"
-                minlength="8"
-                v-model="passwordConfirmation"
-                required
-              />
-              <label for="confirmation_password">確認用パスワード</label>
-            </div>
-          </div>
-          <div class="row register-admin-btn">
-            <button class="btn" type="button" v-on:click="registerUser">
-              <span>登録<i class="material-icons right">done</i></span>
-            </button>
+            <div><button type="button" v-on:click="reset">クリア</button></div>
           </div>
         </div>
       </div>
-    </div>
+    </form>
   </div>
 </template>
 
@@ -184,8 +182,16 @@ export default class XXXComponent extends Vue {
       this.errorMailAddress = "メールアドレスが入力されていません";
       this.errorChecker = false;
     }
+    if (!this.mailAddress.indexOf("@")) {
+      this.errorMailAddress = "メールアドレスの形式が不正です";
+      this.errorChecker = false;
+    }
     if (this.zipCode === "") {
       this.errorZipCode = "郵便番号が入力されていません";
+      this.errorChecker = false;
+    }
+    if (/^[0-9]{3}-[0-9]{4}$/.test(this.zipCode) === false) {
+      this.errorZipCode = "郵便番号はXXX-XXXXの形式で入力してください";
       this.errorChecker = false;
     }
     if (this.address === "") {
@@ -196,8 +202,17 @@ export default class XXXComponent extends Vue {
       this.errorTelephone = "電話番号が入力されていません";
       this.errorChecker = false;
     }
+    if (/\d{2,4}-\d{2,4}-\d{4}/.test(this.telephone) === false) {
+      this.errorTelephone = "電話番号はXXXX-XXXX-XXXXの形式で入力してください";
+      this.errorChecker = false;
+    }
     if (this.password === "") {
       this.errorPassword = "パスワードが入力されていません";
+      this.errorChecker = false;
+    }
+    if (this.password.length < 8 || 16 < this.password.length) {
+      this.errorPassword =
+        "パスワードは８文字以上１６文字以内で設定してください";
       this.errorChecker = false;
     }
     if (this.passwordConfirmation === "") {
@@ -205,7 +220,7 @@ export default class XXXComponent extends Vue {
       this.errorChecker = false;
     }
     if (this.password != this.passwordConfirmation) {
-      this.errorPassword = "パスワードと確認用パスワードが一致していません";
+      this.errorPassword = "パスワードと確認用パスワードが不一致です";
     }
 
     if (this.errorChecker === false) {
@@ -219,11 +234,20 @@ export default class XXXComponent extends Vue {
         "そのメールアドレスはすでに使われています。"
       )
     ) {
-      this.errorMessage =
-        "メールアドレスがすでに使われているため、登録できませんでした";
+      this.errorMailAddress = "そのメールアドレスはすでに使われています";
     } else {
       this.errorMessage = "登録できませんでした";
     }
+  }
+  reset(): void {
+    this.lastName = "";
+    this.firstName = "";
+    this.mailAddress = "";
+    this.zipCode = "";
+    this.address = "";
+    this.telephone = "";
+    this.password = "";
+    this.passwordConfirmation = "";
   }
 }
 </script>
