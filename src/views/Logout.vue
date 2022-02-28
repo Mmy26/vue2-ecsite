ff690e13e5b48a54620456ff91241f8bc1af2f3c
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-02-28 14:55:48 +0900

    ログアウト機能の追加

--- src/views/Logout.vue
+++ src/views/Logout.vue
@@ -1,17 +1,20 @@
 <template>
-  <div>
-    
-  </div>
+  <div></div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import axios from "axios";
 @Component
 export default class XXXComponent extends Vue {
-
+  async logoutUser(): Promise<void> {
+    const response = await axios.post(
+      "https://153.127.48.168:8080/ecsite-api/user/logout"
+    );
+    console.dir("response:" + JSON.stringify(response));
+    this.$router.push("/itemList");
+  }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
