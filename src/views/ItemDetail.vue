8b69bcc0edfd07fd576c225de79663ac98bd03e4
kanacoyn <knkyn723@gmail.com>
2022-02-28 10:49:06 +0900

    itemDetail.vueにwebAPIを読み込む処理

--- src/views/ItemDetail.vue
+++ src/views/ItemDetail.vue
@@ -1,13 +1,37 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div class="top-wrapper">
+      <router-link to="/itemList" class="breadcrumb">商品一覧</router-link>
+    </div>
+    <form>
+      <button type="button" v-on:click="addToCart">カートに入れる</button>
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import { Order } from "@/type/order";
 import axios from "axios";
+import { OrderItem } from "@/type/orderItem";
+import { orderTopping } from "@/type/OrderTopping";
+import { Item } from "@/type/item";
+import { Topping } from "@/type/topping";
 @Component
-export default class ItemDetail extends Vue {}
+export default class ItemDetail extends Vue {
+  private currentItem = new Item(
+    0,
+    "",
+    "",
+    "",
+    0,
+    0,
+    "",
+    false,
+    new Topping(0, "", "", 0, 0)
+  );
+  private currentItemImage = "";
+}
 </script>
 
 <style scoped></style>
