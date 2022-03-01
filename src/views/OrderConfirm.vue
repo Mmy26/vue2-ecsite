72ccbf97872b086056319e8cd0e06a0eb0e1cd58
ryota-09 <n.ryo950911@gmail.com>
2022-03-01 10:54:09 +0900

    各フォームのvalidation設定実装

--- src/views/OrderConfirm.vue
+++ src/views/OrderConfirm.vue
@@ -10,6 +10,7 @@ import { User } from "@/type/user";
 import { OrderItem } from "@/type/orderItem";
 import { Order } from "@/type/order";
 import  axios  from "axios";
+import {getHours} from "date-fns"
 
 @Component
 export default class OrderConfirm extends Vue {
@@ -21,9 +22,89 @@ export default class OrderConfirm extends Vue {
   private destinationTel = "";
   private deliveryTime = new Date();
   private paymentMethod = 0;
+  private hasError = false;
   private errorMessage = "";
+  private nameErrorMessage = "";
+  private emailErrorMessage = "";
+  private zipcodeErrorMessage = "";
+  private addressErrorMessage = "";
+  private telErrorMessage = "";
+  private delivelyErrorMessage = "";
 
   async order(): Promise<void> {
+    if( this.destinationName === "" ){
+      this.nameErrorMessage = "名前を入力して下さい";
+      this.hasError = true;
+    }
+    const includeOrNot = (str: string):boolean => {
+      return this.destinationEmail.includes(str);
+    }
+    if ( this.destinationEmail === ""){
+      this.emailErrorMessage = "メールアドレスを入力してください。"
+      this.hasError = true;
+    }
+    if (includeOrNot("@")) {
+      this.emailErrorMessage = "メールアドレスの形式が不正です。"
+      this.hasError = true;
+    }
+    const addressCheck = (): boolean => {
+      let hasAddressError = false;
+      let targetArray = new Array<string>();
+      targetArray = this.destinationZipcode.split("-");
+      if (targetArray[0].length != 3){
+        hasAddressError = true;
+      } else if ( targetArray[1].length != 4 ){
+        hasAddressError = true;
+      }
+      return hasAddressError;
+    }
+    if( addressCheck() ){
+      this.hasError = true;
+      this.zipcodeErrorMessage = "郵便番号はXXX-XXXXの形式で入力してください"
+    }
+    if( this.destinationZipcode === "" ){
+        this.zipcodeErrorMessage = "住所を入力してください。"
+    }
+    if( this.destinationAddress === "" ){
+      this.hasError = true;
+      this.addressErrorMessage = "住所を入力してください。"
+    }
+    const telCheck = (): boolean => {
+      let hasTelError = false;
+      let targetArray = new Array<string>();
+      if(includeOrNot("-")){
+        hasTelError = true;
+      }
+      targetArray = this.destinationTel.split("-");
+      if (targetArray[0].length != 4){
+        hasTelError = true;
+      } else if ( targetArray[1].length != 4 ){
+        hasTelError = true;
+      } else if ( targetArray[2].length != 4 ){
+        hasTelError = true;
+      }
+      return hasTelError;
+    }
+    if( telCheck() ){
+      this.telErrorMessage = "電話番号はXXXX-XXXX-XXXXの形式で入力してください";
+      this.hasError = true;
+    }
+    if( this.destinationTel === "電話番号を入力してください。" ){
+      this.telErrorMessage = "電話番号を入力してください。";
+      this.hasError = true;
+    }
+    if( getHours(this.deliveryTime) <= getHours( new Date()) + 3 ){
+      this.delivelyErrorMessage = "今から3時間後の日時をご入力ください";
+      this.hasError = true;
+    }
+    if( this.deliveryTime === new Date() ){
+      this.delivelyErrorMessage = "配達日時を入力してください。"
+    }
+    if(this.hasError){
+      return;
+    }
+
+
     const response = await axios.post(
       "http://153.127.48.168:8080/ecsite-api/order",
       {
