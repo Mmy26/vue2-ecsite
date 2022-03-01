0ab1152ba805c1b9669d98579c6b890810e14ba5
ryota-09 <n.ryo950911@gmail.com>
2022-03-01 12:48:11 +0900

    ファイルの作成

---
+++ src/components/CompCreditCardPayment.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="credit-card-component">
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class CompCreditCardPayment extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
