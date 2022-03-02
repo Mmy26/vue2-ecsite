baa4fd7db325625ab27fc6b8ea81e5adcf56178c
ryota-09 <n.ryo950911@gmail.com>
2022-03-02 12:21:39 +0900

    マージする

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,6 +5,27 @@
         <img src="img_coffee/header_logo.png" />
         <p>優雅な時間ををあなたにお届け！</p>
       </div>
+      <div class="container">
+        <div class="row">
+          <div class="col s12 l6">
+            <h5>タイトル</h5>
+            <p>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。</p>
+            <p>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。</p>
+          </div>
+          <div class="col s12 l4 l4 offset-l2">
+            <h5>タイトル</h5>
+            <ul>
+              <li class="grey-text text-lighten-3">ここにテキストが入ります。</li>
+              <li class="grey-text text-lighten-3">ここにテキストが入ります。</li>
+              <li class="grey-text text-lighten-3">ここにテキストが入ります。</li>
+              <li class="grey-text text-lighten-3">ここにテキストが入ります。</li>
+            </ul>
+          </div>
+        </div>
+        <div class="footer-copyright grey darken-4">
+          <div class="container center-align">&copy; 2021 ここにテキストが入ります。</div>
+        </div>
+      </div>
     </footer>
   </div>
 </template>
