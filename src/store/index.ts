9bcc51ab385d58304bb73bb12900ad33ae552a10
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-03-02 12:19:43 +0900

    ログイン状態の確認機能の追加（途中）

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,29 +1,48 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import { Item } from '@/type/item'
+import Vue from "vue";
+import Vuex from "vuex";
+import { Item } from "@/type/item";
 import { Order } from "@/type/order";
-import { User } from '@/type/user';
-import { OrderItem } from '@/type/orderItem';
+import { User } from "@/type/user";
+import { OrderItem } from "@/type/orderItem";
 import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    order: new Order(0, 0, 0, 0, new Date(), "", "", "", "", "", new Date(), 0, new User(0, "", "", "", "", "", ""), new Array<OrderItem>()),
+    order: new Order(
+      0,
+      0,
+      0,
+      0,
+      new Date(),
+      "",
+      "",
+      "",
+      "",
+      "",
+      new Date(),
+      0,
+      new User(0, "", "", "", "", "", ""),
+      new Array<OrderItem>()
+    ),
     itemList: new Array<Item>(),
+    // ログインされているかどうか
+    isLogin: false,
   },
   actions: {
     /**
      * 商品リストを非同期通信で取得するメソッド.
      * @param context - コンテクスト
      */
-    async asyncGetItemList(context){
-      const response = await axios.get("http://153.127.48.168:8080/ecsite-api/item/items/coffee");
+    async asyncGetItemList(context) {
+      const response = await axios.get(
+        "http://153.127.48.168:8080/ecsite-api/item/items/coffee"
+      );
       console.dir("response: " + JSON.stringify(response));
       const payload = response.data;
       context.commit("showItemList", payload);
-    }
+    },
   },
   mutations: {
     /**
@@ -31,10 +50,10 @@ export default new Vuex.Store({
      * @param state - ステイト
      * @param payload - 商品一覧情報のペイロード
      */
-    showItemList(state, payload){
+    showItemList(state, payload) {
       state.itemList = new Array<Item>();
       console.log(payload.items);
-      for(const item of payload.items){
+      for (const item of payload.items) {
         state.itemList.push(
           new Item(
             item.id,
@@ -50,7 +69,7 @@ export default new Vuex.Store({
         );
       }
     },
-    changeOrderStatus(state, payload){
+    changeOrderStatus(state, payload) {
       // const statusList = {
       //   targetKey: payload.key,
       //   targetValue: payload.value
@@ -58,16 +77,29 @@ export default new Vuex.Store({
       // state.order.statusList.targetKey = statusList.targetValue;
       state.order.status = payload.status;
     },
-    updateOrder(state, payload){
+    updateOrder(state, payload) {
       state.order.destinationName = payload.destinationName;
       state.order.destinationEmail = payload.destinationEmail;
       state.order.destinationZipcode = payload.destinationZipcode;
       state.order.destinationAddress = payload.destinationAddress;
       state.order.destinationTel = payload.destinationTel;
-    }
+    },
+    /**
+     * ログインする.
+     * @param state ステート
+     */
+    logined(state) {
+      state.isLogin = true;
+    },
+    /**
+     * ログアウトする.
+     * @param state ステート
+     */
+    logouted(state) {
+      state.isLogin = false;
+    },
   },
-  modules: {
-  },
+  modules: {},
   getters: {
     /**
      * 商品一覧を表すgetter.
@@ -76,6 +108,14 @@ export default new Vuex.Store({
      */
     getItemList(state) {
       return state.itemList;
-    }
-  }
-})
+    },
+    /**
+     * ログイン状態を表示する.
+     * @param state - ステート
+     * @returns ログイン状態
+     */
+    getLoginStatus(state) {
+      return state.isLogin;
+    },
+  },
+});
